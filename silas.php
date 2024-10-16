3657294febd520dd3a1f0a978adfd611bc5b101f
cooukiez <cookiecenter@web.de>
2024-10-16 10:38:29 +0200

    silas version working

--- silas.php
+++ silas.php
@@ -1,29 +1,24 @@
 <?php
 require 'vendor/autoload.php'; // Autoload for php-ics-parser
 
-use Kigkonsult\Icalcreator\Vcalendar;
+use ICal\ICal;
 
 // Fetch and parse the .ics file
 $ics_url = 'https://iserv.kkg.berlin/iserv/public/calendar?key=eb329e8158c8ef6188a3cc2de8740c5b';
 $ics_content = file_get_contents($ics_url);
 file_put_contents('calendar.ics', $ics_content);
 
-$file = 'calendar.ics';
-$vcalendar = Vcalendar::factoryFromFile($file);
+// Parse the .ics file
+$ical = new ICal('calendar.ics');
+$events = $ical->events();
 
-// Loop through events
-foreach ($vcalendar->getComponents() as $component) {
-    if ('vevent' === strtolower($component->getCompType())) {
-        $summary = $component->getProperty('summary');
-        $start = $component->getProperty('dtstart');
-        $end = $component->getProperty('dtend');
-
-        echo "Event: $summary\n";
-        echo "Start: " . $start['value'] . "\n";
-        echo "End: " . $end['value'] . "\n";
-    }
+// Store event data in a simple array for easier HTML rendering
+$eventData = [];
+foreach ($events as $event) {
+    $eventData[] = [
+        'summary' => $event->summary,
+        'dtstart' => $event->dtstart,
+        'dtend'   => $event->dtend,
+        'location' => $event->location
+    ];
 }
-
-
-// Pass the event data to the HTML template
-include 'calendar_template.php';
