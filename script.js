f1cbf4ef3aed6485d2ce8464539ccc9a9f15c580
7Hugo7 <144454004+7Hugo7@users.noreply.github.com>
2024-10-16 15:29:08 +0200

    fullcal

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid'; // For month view
 import iCalendarPlugin from '@fullcalendar/icalendar'; // For .ics support
 
+document.addEventListener('DOMContentLoaded', function() {
 // Target the div where the calendar will be rendered
-const calendarEl = document.getElementById('calendar');
+    const calendarEl = document.getElementById('calendar');
 
-const calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin, iCalendarPlugin], // Include plugins for month view and iCalendar
-    initialView: 'dayGridMonth', // Month view by default
-    events: {
-        url: 'https://iserv.kkg.berlin/iserv/public/calendar?key=eb329e8158c8ef6188a3cc2de8740c5b', // URL to the .ics file
-        format: 'ics' // Let FullCalendar know it's an .ics file
-    }
-});
+    const calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin, iCalendarPlugin], // Include plugins for month view and iCalendar
+        initialView: 'dayGridMonth', // Month view by default
+        events: {
+            url: 'https://iserv.kkg.berlin/iserv/public/calendar?key=eb329e8158c8ef6188a3cc2de8740c5b', // URL to the .ics file
+            format: 'ics' // Let FullCalendar know it's an .ics file
+        }
+    });
 
 // Render the calendar
-calendar.render();
+    calendar.render()
+})
